5d22bc21539362f4b2ec0339676d8cf6f6f1f887
FluffyK <43128163+FluffyK@users.noreply.github.com>
2018-12-03 11:23:54 +0200

    Delete LAB5-2.sql

--- LAB5-2.sql
+++
@@ -1,8 +0,0 @@
-declare @faranota1 int = 6, @faranota2 int = 8;
-select  top (10) Nume_Student, Prenume_Student, Nota
-from studenti, studenti_reusita, discipline
-where discipline.Id_Disciplina = studenti_reusita.Id_Disciplina
-and studenti.Id_Student = studenti_reusita.Id_Student
-and Disciplina='Baze de date'
-and Tip_Evaluare='Testul 1'
-and Nota = iif(Nota != @faranota1 and Nota != @faranota2, Nota, 0);
